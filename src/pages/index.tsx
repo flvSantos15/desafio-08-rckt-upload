5a1752091e440d9396f1d6039198817f2fb2d8fe
Flavio Santos <flvSantos300@gmail.com>
2022-06-03 21:57:55 -0300

    first config done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,38 @@ import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
+import { number } from 'yup/lib/locale';
 import { Header } from '../components/Header';
 import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Images {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface GetImageResponse {
+  after: string;
+  data: Images[];
+}
+
 export default function Home(): JSX.Element {
+  // crio uma func que recebe um parametro e retorna a response da api
+  async function fecthImages({ pageParam = null }): Promise<GetImageResponse> {
+    const { data } = await api.get(`/api/images`, {
+      // passo no parametro o que a func receber senão não passo nada
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -16,12 +41,11 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fecthImages, {
+    getNextPageParam: lastPage => lastPage?.after || null,
+  });
+
+  // parei no 12:00
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
